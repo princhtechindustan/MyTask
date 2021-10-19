40d9ecea440b1385a9a8ae903c2919873046b50d
princhtechindustan <91650237+princhtechindustan@users.noreply.github.com>
2021-10-19 10:16:38 +0530

    Update home.dart

--- my_task/lib/home.dart
+++ my_task/lib/home.dart
@@ -7,8 +7,12 @@ import 'package:my_task/model.dart';
 class HomePage extends StatefulWidget {
   final List<Model> users;
   final Function(Model) onDelete;
-  HomePage({Key? key, required this.users, required this.onDelete})
+   HomePage({Key? key, required this.users, required this.onDelete})
       : super(key: key);
+
+  // HomePage({Key? key, })
+  //     : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -19,12 +23,10 @@ class _HomePageState extends State<HomePage> {
       widget.onDelete(user);
     });
   }
-
-  List<Model> userList = [];
-
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff021BFBE),
+      backgroundColor: Color(0xff021bfbe),
       appBar: AppBar(
         title: const Padding(
           padding: EdgeInsets.all(8.0),
@@ -35,123 +37,153 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Stack(
         children: [
-          Column(
-            children: [
-              Expanded(
-                flex: 1,
+          margin_20,
+          Padding(
+            padding: const EdgeInsets.only(top: 80, right: 20, left: 40),
+            child: RichText(
+              text: TextSpan(
+                text: 'Healthy',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: GoogleFonts.roboto(
+                    fontWeight: FontWeight.w900,
+                  ).fontFamily,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: ' Fruits',
+                    style: TextStyle(
+                      color: Color(0xff064DBDC),
+                      fontSize: 25,
+                      fontWeight: FontWeight.w900,
+                      fontFamily: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w900,
+                      ).fontFamily,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 150),
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(100),
+                ),
+                color: Colors.white,
+              ),
+              child: SingleChildScrollView(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    margin_50,
-                    const SizedBox(height: 30,),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 50),
-                      child: RichText(
-                        text: TextSpan(
-                            text: 'Healthy',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 25,
-                              fontWeight: FontWeight.w900,
-                              fontFamily: GoogleFonts.roboto(
-                                fontWeight: FontWeight.w900,
-                              ).fontFamily,
-                            ),
-                            children: <TextSpan>[
-                              TextSpan(
-                                text: ' Fruits',
-                                style: TextStyle(
-                                  color: Color(0xff064DBDC),
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.w900,
-                                  fontFamily: GoogleFonts.roboto(
-                                    fontWeight: FontWeight.w900,
-                                  ).fontFamily,
+                    Container(
+                      margin: const EdgeInsets.all(50),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ListView.builder(
+                            shrinkWrap: true,
+                            itemBuilder: (BuildContext context, int index) =>
+                                Card(
+                              elevation: 8,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'Fruits Cost : ${widget.users[index].name}',
+                                          style: const TextStyle(fontSize: 12),
+                                        ),
+                                        Text(
+                                          'Fruits total price: ${widget.users[index].price}',
+                                          style: const TextStyle(fontSize: 12),
+                                        ),
+                                      ],
+                                    ),
+                                    IconButton(
+                                      icon: const Icon(Icons.delete),
+                                      onPressed: () =>
+                                          widget.onDelete(widget.users[index]),
+                                    )
+                                  ],
                                 ),
-                              )
-                            ]),
+                              ),
+                            ),
+                            itemCount: widget.users.length,
+                          ),
+                          // ListView.builder(
+                          //   shrinkWrap: true,
+                          //   itemBuilder: (BuildContext context, int index) =>
+                          //       Card(
+                          //         elevation: 8,
+                          //         child: Padding(
+                          //           padding: const EdgeInsets.all(16),
+                          //           child: Row(
+                          //             mainAxisAlignment:
+                          //             MainAxisAlignment.spaceBetween,
+                          //             children: <Widget>[
+                          //               Column(
+                          //                 crossAxisAlignment:
+                          //                 CrossAxisAlignment.start,
+                          //                 children: [
+                          //                   Text(
+                          //                     'Fruits Cost : ${userList[index].name}',
+                          //                     style: const TextStyle(fontSize: 12),
+                          //                   ),
+                          //                   Text(
+                          //                     'Fruits total price: ${userList[index].price}',
+                          //                     style: const TextStyle(fontSize: 12),
+                          //                   ),
+                          //                 ],
+                          //               ),
+                          //               IconButton(
+                          //                 icon: const Icon(Icons.delete),
+                          //                 onPressed: () =>
+                          //                     widget.onDelete(userList[index]),
+                          //               )
+                          //             ],
+                          //           ),
+                          //         ),
+                          //       ),
+                          //   itemCount: userList.length,
+                          // ),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-              Expanded(
-                flex: 4,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(130),
-                    ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: FloatingActionButton(
+                  child: const Icon(
+                    Icons.add,
                     color: Colors.white,
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin:
-                            const EdgeInsets.only(left: 40, right: 40, top: 20),
-                        child: Padding(
-                          padding: const EdgeInsets.all(15),
-                          child: Column(
-                            children: [
-                              // Text(widget.dataList.name.toString()),
-                              // Text(widget.dataList.price.toString()),
-                              // Card(
-                              //   elevation: 5,
-                              //   child: ListTile(
-                              //     leading: Icon(Icons.fastfood),
-                              //     title: Text(widget.dataList.name.toString()),
-                              //     subtitle:
-                              //         Text(widget.dataList.price.toString()),
-                              //     trailing: Icon(Icons.food_bank),
-                              //   ),
-                              // ),
-                              ListView.builder(
-                                shrinkWrap: true,
-                                itemBuilder:
-                                    (BuildContext context, int index) => Card(
-                                  elevation: 8,
-                                  child: Padding(
-                                    padding: EdgeInsets.all(16),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                      children: <Widget>[
-                                        Column(
-                                          crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              'Fruits Cost : ${widget.users[index].name}',
-                                              style: TextStyle(fontSize: 12),
-                                            ),
-                                            Text(
-                                              'Fruits total price: ${widget.
-                                              users[index].price}',
-                                              style: TextStyle(fontSize: 12),
-                                            ),
-                                          ],
-                                        ),
-                                        IconButton(
-                                          icon: Icon(Icons.delete),
-                                          onPressed: () => widget.
-                                          onDelete(widget.users[index]),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                itemCount: widget.users.length,
-                              ),
-                            ],
-                          ),
-                        ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DataClass(),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+                    );
+                  }),
+            ),
           ),
         ],
       ),
@@ -193,4 +225,5 @@ class _HomePageState extends State<HomePage> {
   var weightMargin_30 = const SizedBox(
     width: 30,
   );
+
 }
